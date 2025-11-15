3ff93713d424f13e2ec39bc38b1df02d5313f08c
Jabir Yimam <jabiryimam1@gmail.com>
2025-11-15 19:19:56 +0300

    before major token system update

--- contracts/ReputationToken.sol
+++ contracts/ReputationToken.sol
@@ -1,24 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract ReputationToken is ERC20,Ownable {
+
+contract ReputationToken is ERC20, Ownable {
     constructor() ERC20("ReputationToken", "RPT") {
-        mint(msg.sender, 1000000 * 10 ** decimals());
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
     function burn(address from, uint256 amount) public onlyOwner {
         _burn(from, amount);
     }
-
-
-
-
-
-
-
-
-
 }
